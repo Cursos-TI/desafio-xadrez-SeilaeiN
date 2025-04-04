5cef1c028b8b4c5fc1049375950c860832a028db
Henrique Santos <rikowow89@gmail.com>
2025-04-04 01:23:59 -0300

    Desafio Novato - Movimentação Peças de xadrez.c
    Alterando a variavel "i" para "casas" melhorando a clareza do código.

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-// Declaração de variaveis da quantidade de casas para movimentações das peças de xadrez Torre, Bispo e Rainha
+// Declaração de variáveis da quantidade de casas para movimentações das peças de xadrez Torre, Bispo e Rainha
 int main() {
     int casasTorre = 5;
     int casasBispo = 5;
@@ -16,25 +16,25 @@ int main() {
 
     // Executando a Movimentação da Torre utilizando a estrutura For
     printf("\nMovimento da Torre:\n");
-    for (int i = 0; i < casasTorre; i++) {
-        printf("%d Direita\n", i+1);
+    for (int casas = 0; casas < casasTorre; casas++) {
+        printf("%d Direita\n", casas + 1);
     }
 
     // Executando o Movimento do Bispo utilizando a estrutura While
     printf("\nMovimento do Bispo:\n");
-    int i = 0;
-    while (i < casasBispo) {
-        printf("%d Cima, Direita\n", i+1);
-        i++;
+    int casas = 0;
+    while (casas < casasBispo) {
+        printf("%d Cima, Direita\n", casas + 1);
+        casas++;
     }
 
     // Movimento da Rainha utilizando a estrutura do-while
     printf("\nMovimento da Rainha:\n");
-    i = 0;
+    casas = 0;
     do {
-        printf("%d Esquerda\n",i+1);
-        i++;
-    } while (i < casasRainha);
+        printf("%d Esquerda\n", casas + 1);
+        casas++;
+    } while (casas < casasRainha);
 
     return 0;
 }
